ab3fde0b12b8cc3223607d1787c988090af12199
republicdomain <56187472+republicdomain@users.noreply.github.com>
2023-02-12 13:59:37 +0000

    workflow

--- .github/classroom/test2.sh
+++ .github/classroom/test2.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-echo $GITHUB_TOKEN
-
-# Test if the setup-samples.sh exist and does what it should
-if [ -x setup-samples.sh ]
-then
- ./setup-samples.sh
-
-  echo "Looking for './samples/animals/countries/.apes.txt'"
-  cat ./samples/animals/countries/.apes.txt || exit 255
-
-  echo "Looking for './samples/animals/Elephant babies.txt'"
-  cat "./samples/animals/Elephant babies.txt" || exit 255
-
-  exit 0
-else
-  echo "There is no executable file named 'setup-samples.sh'"
-  exit 255
-fi
+gh auth login --with-token < $(echo $GITHUB_TOKEN)
+gh issue list -s all
